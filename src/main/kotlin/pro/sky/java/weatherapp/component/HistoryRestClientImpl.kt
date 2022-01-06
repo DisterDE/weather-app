c292fd9bb8001af3c5137b8c955033d24a97b258
Dister <disterko@gmail.com>
2022-01-06 20:53:01 +0300

    small changes

--- src/main/kotlin/pro/sky/java/weatherapp/component/HistoryRestClientImpl.kt
+++ src/main/kotlin/pro/sky/java/weatherapp/component/HistoryRestClientImpl.kt
@@ -1,10 +1,9 @@
 package pro.sky.java.weatherapp.component
 
-import kotlinx.coroutines.flow.first
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.bodyToFlow
+import org.springframework.web.reactive.function.client.awaitBody
 import pro.sky.java.weatherapp.domain.HistoryRecord
 
 @Component
@@ -14,11 +13,11 @@ class HistoryRestClientImpl : HistoryRestClient {
     private lateinit var historyUrl: String
 
     override suspend fun saveRecord(record: HistoryRecord): HistoryRecord {
-        return WebClient.create(historyUrl)
+        return WebClient.create()
             .post()
+            .uri(historyUrl)
             .bodyValue(record)
             .retrieve()
-            .bodyToFlow<HistoryRecord>()
-            .first()
+            .awaitBody()
     }
 }
